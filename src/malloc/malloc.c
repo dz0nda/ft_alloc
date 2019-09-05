07a656a14e39b247ba5ed134b0cf233e2dc6f4f4
dz0nda <dzonda@student.42.fr>
2019-09-05 23:13:55 +0200

    - add: malloc basic e2e works

--- src/malloc/malloc.c
+++ src/malloc/malloc.c
@@ -15,11 +15,22 @@
 
 void	*ft_malloc(size_t size)
 {
-	t_node	**arena;
+	t_node	**head;
+	t_node	*node;
 
-	arena = NULL;
+	head = NULL;
 	size = ft_alloc_get_size_aligned(size, FT_ALLOC_ALIGNMENT);
-	arena = ft_alloc_get_arena(size);
-
+	head = ft_alloc_get_arena(size);
+	if ((node = ft_alloc_search(head, size)) == NULL)
+	{
+		if (ft_alloc_arena(head, ft_alloc_get_size_arena(size)) == EXIT_FAILURE)
+			return (NULL);
+		if ((node = ft_alloc_search(head, size)) == NULL)
+			return (NULL);
+	}
+	if (node->size > (size + FT_ALLOC_SIZE_NODE))
+		ft_alloc_split_node(node, size);
+	node->size = size;
+	node->free = FALSE;
 	return (NULL);
 }
