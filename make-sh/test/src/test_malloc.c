d18eac1d1a02c89777912492325eee0af6fd0ba6
dz0nda <dzonda@student.42.fr>
2019-09-07 09:42:36 +0200

    - add: test

--- make-sh/test/src/test_malloc.c
+++ make-sh/test/src/test_malloc.c
@@ -5,13 +5,15 @@ void test_malloc()
 	printf("\n===== start test_malloc =====\n");
 
 	ft_malloc(20);
-		ft_malloc(20);
+	 ft_malloc(20);
 
-	// ft_malloc(20);
-	// ft_malloc(400);
-	// ft_malloc(350);
-	// ft_malloc(45);
-	// ft_malloc(3500);
+	 ft_malloc(20);
+	  ft_malloc(400);
+	 ft_malloc(350);
+	ft_malloc(45);
+	 ft_malloc(3500);
+
+	ft_show_alloc_info();
 
 	show_alloc_mem();
 	printf("\n");
