039eb9220a456a20eb9d9df312217d70834fd19c
dz0nda <dzonda@student.42.fr>
2019-09-09 14:46:48 +0200

    - fix: free

--- src/free/free.c
+++ src/free/free.c
@@ -22,63 +22,43 @@
 //         munmap(g_allocinfo.arena[arena_size], arena_size_map);
 // }
 
-int     ft_alloc_is_alloc(t_anode *node, FT_ALLOC_UINT ptr)
+
+int     ft_alloc_is_concat(t_anode *node)
 {
-    FT_ALLOC_UINT addr;
+    FT_ALLOC_UINT addr_prev;
+    FT_ALLOC_UINT addr_next;
 
-    addr = (FT_ALLOC_UINT)node;
-    printf("Addr = %ld\n", addr);
-    // if (addr < ptr && ptr < addr + node->size)
-    if ((addr + FT_ALLOC_SIZE_NODE) == ptr)
-        return (EXIT_SUCCESS);
-    return (EXIT_FAILURE);
-}
-
-t_anode    *ft_alloc_search_address(FT_ALLOC_UINT ptr)
-{
-    t_aarena arena;
-    t_anode *head;
-    t_anode *node;
-    size_t size;
-
-    arena = -1;
-    head = NULL;
-    node = NULL;
-    size = 0;
-    printf("PTR = %ld \n", ptr);
-    while (++arena < ALLOC_NONE)
+    addr_prev = (FT_ALLOC_UINT)node->prev;
+    addr_next = (FT_ALLOC_UINT)node->next;
+    if ((node->prev < node) && (node->prev->free == TRUE))
     {
-        head = g_alloc_state.alloc_arena[arena];
-        node = head;
-        while((node = node->next) != head && node->free == FALSE)
-            if (ft_alloc_is_alloc(node, ptr) == EXIT_SUCCESS)
-                return (node);
-        if (ft_alloc_is_alloc(node, ptr) == EXIT_SUCCESS)
-            return (node);
+        node->prev->size += node->size + FT_ALLOC_SIZE_NODE;
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        node = node->prev;
     }
-    return (NULL);
+    if ((node->next > node) && (node->next->free == TRUE))
+    {
+        node->size += node->next->size + FT_ALLOC_SIZE_NODE;
+        node->next = node->next->next;
+        node->next->prev = node;
+    }
+    return (EXIT_SUCCESS);
 }
 
 void    ft_free(void *ptr)
 {
+    FT_ALLOC_UINT addr;
     t_anode *node;
-
-    FT_ALLOC_UINT size = (FT_ALLOC_UINT)ptr;
-    printf("size = %ld \n", size);
-
-    if ((node = ft_alloc_search_address((FT_ALLOC_UINT)ptr)) == NULL)
-    {
-        printf("here in free");
+    
+    addr = (FT_ALLOC_UINT)ptr;
+    if (ptr == NULL || addr == 0)
+        return ;
+    if ((node = ft_alloc_search_node_by_address(addr)) == NULL)
             return ;
-    }
+    ft_alloc_is_concat(node);
     node->free = TRUE;
-    // t_node *node;
 
-    // node = NULL;
-    // node = (t_node *)((FT_ALLOC_PTR_UINT)ptr - sizeof(t_node));
-    // if (node->free != TRUE && node->free != FALSE)
-    //     return ;
-    // node->free = TRUE;
     // ft_alloc_update_total(node->size, node->free);
     // ft_alloc_merge_node(node);
     // ft_alloc_total_prune(node->size);
