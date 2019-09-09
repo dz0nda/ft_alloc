1a0e82f8a895dc3385b881bca1fe6854af0baccd
dz0nda <dzonda@student.42.fr>
2019-09-09 14:45:25 +0200

    - refactor: get

--- src/alloc/alloc_get.c
+++ src/alloc/alloc_get.c
@@ -5,7 +5,16 @@ size_t	ft_alloc_get_size_aligned(size_t offset, size_t align)
 	return (offset + (align - (offset % align) % align));
 }
 
-size_t	ft_alloc_get_size_arena(size_t size)
+t_aarena  ft_alloc_get_arena_index_by_size(size_t size)
+{
+  if (size <= FT_ALLOC_TINY)
+    return (ALLOC_TINY);
+  else if (size > FT_ALLOC_TINY && size <= FT_ALLOC_SMALL)
+    return (ALLOC_SMALL);
+  return (ALLOC_LARGE);
+}
+
+size_t  ft_alloc_get_arena_size_by_size(size_t size)
 {
   if (size <= FT_ALLOC_TINY)
     return (FT_ALLOC_N);
@@ -14,20 +23,11 @@ size_t	ft_alloc_get_size_arena(size_t size)
   return (ft_alloc_get_size_aligned(size, FT_ALLOC_PAGESIZE));
 }
 
-t_anode  **ft_alloc_get_arena(size_t size)
+t_anode  **ft_alloc_get_arena_by_size(size_t size)
 {
   if (size <= FT_ALLOC_TINY)
     return (&(g_alloc_state.alloc_arena)[ALLOC_TINY]);
   else if (size > FT_ALLOC_TINY && size <= FT_ALLOC_SMALL)
     return (&(g_alloc_state.alloc_arena)[ALLOC_SMALL]);
   return (&(g_alloc_state.alloc_arena)[ALLOC_LARGE]);
-}
-
-t_aarena  ft_alloc_get_target(size_t size)
-{
-  if (size <= FT_ALLOC_TINY)
-    return (ALLOC_TINY);
-  else if (size > FT_ALLOC_TINY && size <= FT_ALLOC_SMALL)
-    return (ALLOC_SMALL);
-  return (ALLOC_LARGE);
 }
