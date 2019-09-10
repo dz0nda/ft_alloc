9c90aa9bba14e34f98d936332fb3b076a8150917
dz0nda <dzonda@student.42.fr>
2019-09-10 16:27:20 +0200

    - refactor: malloc

--- src/malloc/malloc.c
+++ src/malloc/malloc.c
@@ -15,21 +15,22 @@
 
 void	*ft_malloc(size_t size)
 {
-	t_anode	**head;
 	t_anode	*node;
 
-	head = NULL;
 	node = NULL;
 	if (g_alloc_state.init == FALSE && ft_alloc_init() == EXIT_FAILURE)
 		return (NULL);
 	size = ft_alloc_get_size_aligned(size, FT_ALLOC_ALIGNMENT);
-	head = ft_alloc_get_arena_by_size(size);
 	if ((node = ft_alloc_search_node_by_size(size)) == NULL)
-		if ((node = ft_alloc_arena_new(size)) == NULL)
+		if ((node = ft_alloc_arena_new_by_size(size)) == NULL)
 			return (NULL);
-	if (node->size > (size + FT_ALLOC_SIZE_NODE + FT_ALLOC_ALIGNMENT))
-		ft_alloc_arena_split(node, size);
-	// ft_ainfo_rall(ft_alloc_get_target(node->size), node->size, FALSE);
+	if (ft_alloc_arena_split(node, size) == EXIT_SUCCESS)
+	{
+		printf("here");
+		ft_alloc_info_free(node->size, FALSE);
+
+	}
+	ft_alloc_info_used_free(node->size, FALSE);
 	node->free = FALSE;
 	return ((void *)(node + 1));
 }
