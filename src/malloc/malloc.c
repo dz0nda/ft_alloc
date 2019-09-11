7dac4a47810fdf3265c061c467824ae9964a3002
dz0nda <dzonda@student.42.fr>
2019-09-11 20:35:49 +0200

    - minor: fix typo

--- src/malloc/malloc.c
+++ src/malloc/malloc.c
@@ -21,15 +21,11 @@ void	*ft_malloc(size_t size)
 	if (g_alloc_state.init == FALSE && ft_alloc_init() == EXIT_FAILURE)
 		return (NULL);
 	size = ft_alloc_get_size_aligned(size, FT_ALLOC_ALIGNMENT);
-	if ((node = ft_alloc_search_node_by_size(size)) == NULL)
-		if ((node = ft_alloc_arena_new_by_size(size)) == NULL)
+	if ((node = ft_alloc_search_by_size(size)) == NULL)
+		if ((node = ft_alloc_arena(size)) == NULL)
 			return (NULL);
-	if (ft_alloc_arena_split(node, size) == EXIT_SUCCESS)
-	{
-		printf("here");
+	if (ft_alloc_split(node, size) == EXIT_SUCCESS)
 		ft_alloc_info_free(node->size, FALSE);
-
-	}
 	ft_alloc_info_used_free(node->size, FALSE);
 	node->free = FALSE;
 	return ((void *)(node + 1));
