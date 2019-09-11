2ca9237af44d4b9734c3e79e25283a7e3e9ccbfc
dz0nda <dzonda@student.42.fr>
2019-09-11 20:35:22 +0200

    - realloc: WIP

--- src/realloc/realloc.c
+++ src/realloc/realloc.c
@@ -3,19 +3,52 @@
 t_anode     *ft_realloc_fast(t_anode *node, size_t size)
 {
   t_anode   *new;
+  t_aarena   node_idx;
+  t_aarena   new_idx;
 
-    new = node;
+  new = node;
+  node_idx = ft_alloc_get_arena_index_by_size(node->size);
+  new_idx = ft_alloc_get_arena_index_by_size(size);
+
     if (ft_alloc_get_arena_index_by_size(node->size) != 
     ft_alloc_get_arena_index_by_size(size))
       return (NULL);
     if (size < node->size)
-      if (ft_alloc_arena_split(node, size) == EXIT_SUCCESS)
+      if (ft_alloc_split(node, size) == EXIT_SUCCESS)
         return (node);
     else if (node->next->free == TRUE)
       ; //concat right
     else if (node->prev->free == TRUE)
       ;
     return (NULL);
+}
+
+t_anode     *ft_realloc_get_fast(t_anode *node, size_t size)
+{
+  t_anode   *new;
+  t_aarena   node_idx;
+  t_aarena   new_idx;
+
+  new = node;
+  node_idx = ft_alloc_get_arena_index_by_size(node->size);
+  new_idx = ft_alloc_get_arena_index_by_size(size);
+  if (node_idx != new_idx)
+    return (NULL);
+  if (ft_alloc_split(new, size) == EXIT_SUCCESS)
+    return (new);
+  return (NULL);
+  // else
+  // {
+  //   if (node->next->free == TRUE && (node->size + node->next->size) > size)
+
+  // }
+  //   if (ft_alloc_concat(node))
+  
+  //   else if (node->next->free == TRUE)
+  //     ; //concat right
+  //   else if (node->prev->free == TRUE)
+  //     ;
+  //   return (NULL);
 }
 
 void    *ft_realloc(void *ptr, size_t size)
@@ -28,17 +61,22 @@ void    *ft_realloc(void *ptr, size_t size)
     new = NULL;
     addr = (FT_ALLOC_UINT)ptr;
     size = ft_alloc_get_size_aligned(size, FT_ALLOC_ALIGNMENT);
-    if (size == 0)
-      return (NULL);
     if (ptr == NULL || addr == 0)
-        return (ptr = ft_malloc(size));
-    if ((node = ft_alloc_search_node_by_address(addr)) == NULL)
-        return (NULL);
-    if (size == node->size || (node->size > size && node->size - size < FT_ALLOC_ALIGNMENT))
+      return (ft_malloc(size));
+    else if (size == 0)
+      ft_free(ptr);
+    else if ((node = ft_alloc_search_by_address(addr)) == NULL)
       return (ptr);
-    if ((new = ft_realloc_fast(node, size)) == NULL)
-      if ((new = ft_malloc(size)) == NULL)
-        return (NULL);
+    if ((new = ft_realloc_get_fast(node, size)) == NULL)
+      return (NULL);
 
-  return (ptr);
+
+    return (NULL);
+
+  //   if (size == node->size || (node->size > size && node->size - size < FT_ALLOC_ALIGNMENT))
+  //     return (ptr);
+  //   if ((new = ft_realloc_fast(node, size)) == NULL)
+  //     if ((new = ft_malloc(size)) == NULL)
+  //       return (NULL);
+  // return (ptr);
 }
--- src/realloc/realloc.h
+++ src/realloc/realloc.h
@@ -1,7 +1,8 @@
 #ifndef FT_REALLOC_H
 # define FT_REALLOC_H
 
-# include "../alloc/alloc.h"
+# include "../malloc/malloc.h"
+# include "../free/free.h"
 
 void    *ft_realloc(void *ptr, size_t size);
 
