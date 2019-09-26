51e800e2c01e4c68456d7b06dad1dae84e76f581
dz0nda <dzonda@student.42.fr>
2019-09-27 01:21:36 +0200

    - clean: show

--- src/show/ft_show_hexa.c
+++ src/show/ft_show_hexa.c
@@ -6,7 +6,7 @@
 /*   By: dzonda <dzonda@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/09/18 07:39:56 by dzonda       #+#   ##    ##    #+#       */
-/*   Updated: 2019/09/23 21:02:05 by dzonda      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/09/27 01:21:20 by dzonda      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -74,19 +74,19 @@ static char		*ft_itoa(int input, char *buffer, int radix)
 
 void			ft_puthexa(FT_ALLOC_UINT nb)
 {
-	char		s[17];
-	int 		i;
-	int 		tmp;
+	char	s[17];
+	int		i;
+	int		tmp;
 
 	ft_alloc_memset(s, '\0', sizeof(s));
 	i = 0;
 	tmp = 0;
 	ft_putstr("0x");
-	while (nb != 0) 
-	{ 
-			tmp = nb % 16; 
-			s[i++] = (tmp < 10) ? tmp + 48 : tmp + 87;
-			nb /= 16; 
+	while (nb != 0)
+	{
+		tmp = nb % 16;
+		s[i++] = (tmp < 10) ? tmp + 48 : tmp + 87;
+		nb /= 16;
 	}
 	ft_strrev(s);
 	ft_putstr(s);
