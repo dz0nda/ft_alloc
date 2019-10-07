cd1c1e424d5afd9e2deee74735ed2f09daf2dec7
dz0nda <dzonda@student.42.fr>
2019-10-08 00:56:50 +0200

    - refactor: malloc

--- src/malloc/ft_malloc.c
+++ src/malloc/ft_malloc.c
@@ -13,31 +13,34 @@
 
 #include "ft_malloc.h"
 
-void	*malloc(size_t size)
+void	*ft_malloc(size_t size)
 {
 	t_aarena	**arena;
 	t_achunk	*chunk;
-	void		*new;
 
 	arena = NULL;
 	chunk = NULL;
+	size = ft_alloc_get_size_aligned(size, FT_AALIGNMENT);
+	arena = ft_alloc_get_arena_by_size_request(size);
+	if (arena == NULL || (chunk = ft_alloc_search_chunk_by_size(*arena, size)) == NULL)
+		if ((chunk = ft_alloc_arena_new(arena, size)) == NULL)
+			return (NULL);
+	ft_alloc_chunk_split(*arena, chunk, size);
+	ft_alloc_state_swap((*arena)->aindex, chunk->size, FT_FALSE);
+	chunk->free = FT_FALSE;
+	ft_alloc_history_add(chunk, (*arena)->aindex);
+	return ((void *)((FT_AUINT)chunk + g_alloc.info.size_chunk));
+}
+
+void	*malloc(size_t size)
+{
+	void		*new;
+
 	new = NULL;
 	if (ft_alloc_pthread_lock() == EXIT_FAILURE)
 		return (NULL);
 	if (g_alloc.info.pagesize != 0 || ft_alloc_init() == EXIT_SUCCESS)
-	{
-		size = ft_alloc_get_size_aligned(size, FT_ALLOC_ALIGNMENT);
-		arena = ft_alloc_get_arena_by_size_request(size);
-		if (arena == NULL || (chunk = ft_alloc_search_chunk_by_size(*arena, size)) == NULL)
-			chunk = ft_alloc_arena_new(arena, size);
-		if (chunk != NULL)
-		{
-			ft_alloc_chunk_split(*arena, chunk, size);
-			ft_alloc_state_swap((*arena)->aindex, chunk->size, FALSE);
-			chunk->free = FALSE;
-			new = (void *)((FT_ALLOC_UINT)chunk + g_alloc.info.size_chunk);
-		}
-	}
+		new = ft_malloc(size);
 	if (ft_alloc_pthread_unlock() == EXIT_FAILURE)
 		return (NULL);
 	return (new);
