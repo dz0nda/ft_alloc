a35aa499d0cc79ab245d59c4eae68a738692a182
dz0nda <dzonda@student.42.fr>
2019-10-08 04:05:09 +0200

    - refactor: free

--- src/free/ft_free.c
+++ src/free/ft_free.c
@@ -13,27 +13,32 @@
 
 #include "ft_free.h"
 
-void			free(void *ptr)
+static void			ft_free(void *ptr)
 {
 	t_aarena	**arena;
 	t_achunk	*chunk;
 
 	arena = NULL;
 	chunk = NULL;
-	if (ft_alloc_pthread_lock() == EXIT_FAILURE)
-		return ;
 	if ((ptr != NULL) && ((arena = ft_alloc_search_arena_by_address(ptr)) != NULL))
 	{
 		if ((chunk = ft_alloc_search_chunk_by_address(*arena, ptr)) != NULL)
 		{
-			chunk->free = TRUE;
-			ft_alloc_state_swap((*arena)->aindex, chunk->size, TRUE);
+			chunk->free = FT_TRUE;
+			ft_alloc_state_swap((*arena)->aindex, chunk->size, FT_TRUE);
 			ft_alloc_chunk_concat(*arena, chunk);
 			if (((*arena)->head->size + g_alloc.info.size_arena + g_alloc.info.size_chunk) == (*arena)->size)
 				if ((*arena)->prev != NULL || (*arena)->aindex == ALLOC_LARGE)
 					ft_alloc_arena_del(arena);
 		}
 	}
+}
+
+void			free(void *ptr)
+{
+	if (ft_alloc_pthread_lock() == EXIT_FAILURE)
+		return ;
+	ft_free(ptr);
 	if (ft_alloc_pthread_unlock() == EXIT_FAILURE)
 		return ;
 }
