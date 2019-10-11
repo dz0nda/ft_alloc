3ba8962e654c690ffd701b1bdf9b124f6a24eb26
dz0nda <dzonda@student.42.fr>
2019-10-11 23:10:22 +0200

    - clean

--- tests/src/test6.c
+++ tests/src/test6.c
@@ -7,11 +7,11 @@ int		main(void)
 	void *p = malloc(1024);
 	malloc(1024 * 32);
 	void *p2 = malloc(1024 * 1024);
-	// malloc(1024 * 1024 * 16);
+	malloc(1024 * 1024 * 16);
 	void *p3 = malloc(1024 * 1024 * 128);
-	// free(p);
-	// free(p2);
-	// free(p3);
+	free(p);
+	free(p2);
+	free(p3);
 	show_alloc_mem();
 	ft_putstr("\n");
 	show_alloc_mem_details();
