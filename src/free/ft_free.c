e29b2624bdc6ee646f24c3934f2957eaacab289f
dz0nda <dzonda@student.42.fr>
2019-10-21 13:22:21 +0200

    - refactoring: free

--- src/free/ft_free.c
+++ src/free/ft_free.c
@@ -29,7 +29,8 @@ void			free(void *ptr)
 {
 	if (ft_alloc_pthread_lock() == EXIT_FAILURE)
 		return ;
-	ft_free(ptr);
+	if (ft_alloc_init() == EXIT_SUCCESS)
+		ft_free(ptr);
 	if (ft_alloc_pthread_unlock() == EXIT_FAILURE)
 		return ;
 }
