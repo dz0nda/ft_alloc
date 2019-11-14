c06b87c08b5757cea6af3dc4a1e4ee8da23c9e31
dz0nda <dzonda@student.42.fr>
2019-11-14 07:11:55 +0100

    - clean: rendu

---
+++ inc/ft_liballoc.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   liballoc.h                                       .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: dzonda <dzonda@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/09/18 07:45:07 by dzonda       #+#   ##    ##    #+#       */
+/*   Updated: 2019/09/23 21:34:59 by dzonda      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_LIBALLOC_H
+# define FT_LIBALLOC_H
+
+# include "./src/calloc/ft_calloc.h"
+# include "./src/malloc/ft_malloc.h"
+# include "./src/free/ft_free.h"
+# include "./src/realloc/ft_realloc.h"
+# include "./src/reallocf/ft_reallocf.h"
+# include "./src/show/ft_show.h"
+
+#endif
